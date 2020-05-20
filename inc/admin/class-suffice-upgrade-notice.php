ac142f5821667fbefeac57b8c8c133c62a479d43
ThemeGrill <milanthapa299@gmail.com>
2020-05-20 11:07:54 +0545

    Align translation comment with code below

--- inc/admin/class-suffice-upgrade-notice.php
+++ inc/admin/class-suffice-upgrade-notice.php
@@ -77,7 +77,7 @@ class Suffice_Upgrade_Notice extends Suffice_Notice {
 
 				printf(
 					esc_html__(
-					/* Translators: %1$s current user display name., %2$s this theme name., %3$s discount coupon code., %4$s discount percentage. */
+					    /* Translators: %1$s current user display name., %2$s this theme name., %3$s discount coupon code., %4$s discount percentage. */
 						'Howdy, %1$s! You\'ve been using %2$s theme for a while now, and we hope you\'re happy with it. To access more premium features you can always upgrade to pro. All contents and settings will remain as it is after upgrading to pro, you basically start from where you left. Also, you can use the coupon code %3$s to get %4$s discount (limited time offer) while making the purchase. Enjoy! ',
 						'suffice'
 					),
