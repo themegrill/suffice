bce3a1c460d1cfcd1c9ef156d5538be2912d71a4
NituShrestha <shresthanitu984@gmail.com>
2020-07-12 21:32:41 +0545

    escape URL

--- inc/admin/class-suffice-welcome-notice.php
+++ inc/admin/class-suffice-welcome-notice.php
@@ -43,7 +43,7 @@ class Suffice_Welcome_Notice {
 
 			<div class="suffice-message__content">
 				<div class="suffice-message__image">
-					<img class="suffice-screenshot" src="<?php echo get_template_directory_uri(); ?>/screenshot.jpg" alt="<?php esc_html_e( 'Suffice', 'suffice' ); ?>" /><?php // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped, Squiz.PHP.EmbeddedPhp.SpacingBeforeClose ?>
+					<img class="suffice-screenshot" src="<?php echo esc_url( get_template_directory_uri() ); ?>/screenshot.jpg" alt="<?php esc_html_e( 'Suffice', 'suffice' ); ?>" /><?php // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped, Squiz.PHP.EmbeddedPhp.SpacingBeforeClose ?>
 				</div>
 
 				<div class="suffice-message__text">
